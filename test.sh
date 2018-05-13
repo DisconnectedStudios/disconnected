f5cc30c7580be2e00af29c91ee0582b664c0b803
Bar Hofesh <bar.hofesh@gmail.com>
2018-05-13 17:47:25 +0300

    Fixed stuff

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+cd lib/crsfml/ && cmake . && make
+cd ..
+cd ..
+export LIBRARY_PATH="`pwd`/lib/crsfml/voidcsfml"  # Used during linking
 export LD_LIBRARY_PATH="`pwd`/lib/crsfml/voidcsfml"
 crystal src/disconnected.cr --debug --stats        
